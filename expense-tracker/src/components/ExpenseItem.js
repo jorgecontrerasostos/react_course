9266d74807d587792b5a45a4595b7a6a3028a837
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-03-09 16:00:23 -0600

    add css file to ExpenseItem

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
   return (
-    <>
+    <div className="expense-item">
       <div>March 28th, 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
-    </>
+    </div>
   );
 };
 
