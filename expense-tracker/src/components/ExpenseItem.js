951919dbaf5b4976af740bfdd91f365b893139be
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-03-09 15:56:13 -0600

    update ExpenseItem

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+  return (
+    <>
+      <div>March 28th, 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </>
+  );
 };
 
 export default ExpenseItem;
