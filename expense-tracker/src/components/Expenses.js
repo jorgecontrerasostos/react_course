e91d45fe22a09d399344141a7bdde3450e35940d
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-03-10 23:19:42 -0600

    add expenses component

---
+++ expense-tracker/src/components/Expenses.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = (props) => {
+  return (
+    <div className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Expenses;
