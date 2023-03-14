c6117655498597300ff94294959cf1fde39f7f13
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-03-14 08:58:17 -0600

    add inputs for expense

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,9 +1,11 @@
 import Expenses from "./components/expenses/Expenses";
+import NewExpense from "./components/newExpense/NewExpense";
 import expenses from "./data/expenses";
 
 const App = () => {
   return (
     <>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </>
   );
